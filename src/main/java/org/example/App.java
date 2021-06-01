0f5df35499674ed6b5104a3bacba0d02e423f196
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 19:54:19 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Retirement Calculator
+ */
+
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age, ageToRetire, yearsTillRetire, yearOfRetiring;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        // Ask age
+        System.out.print("What is your current age? ");
+        age = scanner.nextInt();
+        System.out.print("At what age would you like to retire? ");
+        ageToRetire = scanner.nextInt();
+
+        yearsTillRetire = ageToRetire - age;
+        yearOfRetiring = year + yearsTillRetire;
+
+        System.out.println("You have " + yearsTillRetire + " Years left until you retire.");
+        System.out.println("It's " + year + ", so you can retire in " + yearOfRetiring + ".");
+
+    }
+}
